f3cad93aea2d863bccf9c77bf8e0889da4e5ca3f
Taha Tehseen <33021614+ttehseen@users.noreply.github.com>
2018-04-27 15:08:16 +0800

    Commit 2

--- js/home.js
+++ js/home.js
@@ -16,17 +16,17 @@ function myFunction() {
 }
 
 function myFunction2() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput2");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL2");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
+    var input2, filter2, ul2, li2, a2, i2;
+    input2 = document.getElementById("myInput2");
+    filter2 = input2.value.toUpperCase();
+    ul2 = document.getElementById("myUL2");
+    li2 = ul2.getElementsByTagName("li2");
+    for (i = 0; i < li2.length; i++) {
+        a2 = li2[i].getElementsByTagName("a2")[0];
+        if (a2.innerHTML.toUpperCase().indexOf(filter2) > -1) {
+            li2[i].style.display = "";
         } else {
-            li[i].style.display = "none";
+            li2[i].style.display = "none";
 
         }
     }
