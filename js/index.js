3ca0064f95578ed151e5a21abcca9645184ba2fd
Taha Tehseen <33021614+ttehseen@users.noreply.github.com>
2018-04-28 17:52:09 +0800

    Commit 11

--- js/index.js
+++ js/index.js
@@ -56,4 +56,23 @@ for(var i = 0; i < 20; i++){
 }
 
 tl.seek(50);
-
+$(function(){
+  $(".increment").click(function(){
+    var count = parseInt($("~ .count", this).text());
+    
+    if($(this).hasClass("up")) {
+      var count = count + 1;
+      
+       $("~ .count", this).text(count);
+    } else {
+      var count = count - 1;
+       $("~ .count", this).text(count);     
+    }
+    
+    $(this).parent().addClass("bump");
+    
+    setTimeout(function(){
+      $(this).parent().removeClass("bump");    
+    }, 400);
+  });
+});
